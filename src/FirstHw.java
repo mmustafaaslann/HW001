71cf649d6c57f02781ab3dcc46260e8ccfe57e15
Mustafa ASLAN <mmustafaslann1453@gmail.com>
2022-09-24 12:23:20 +0300

    hw1bıttı

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -2,5 +2,6 @@ public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
         System.out.println("merhaba dünya");
+        System.out.println("hello world");
     }
 }
